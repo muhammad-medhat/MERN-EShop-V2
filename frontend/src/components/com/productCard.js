1a18956a855338d8a2abec3440ffe3da6ef35237
Muhammad <Muhammad.medhat@gmail.com>
2023-08-09 01:14:57 +0300

    ratings component

--- frontend/src/components/com/productCard.js
+++ frontend/src/components/com/productCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 export const ProductCard = ({ product }) => {
   return (
@@ -9,12 +10,15 @@ export const ProductCard = ({ product }) => {
         <Card.Img src={product.image} variant="top" />
       </Link>
       <Card.Body as="div">
-        <Card.Title as="h4">
+        <Card.Title as="h4" className="product-title">
           <Link to={`/products/${product._id}`}>
             <strong>{product.name}</strong>
           </Link>
         </Card.Title>
-        <Card.Text as="span">${product.price}</Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+        <Card.Text as="span">
+          <Rating value={product.rating} />
+        </Card.Text>
       </Card.Body>
     </Card>
   );
