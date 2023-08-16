36dc7da7387d31c8b04109ae6eed2f3140aa2bac
Muhammad <Muhammad.medhat@gmail.com>
2023-08-17 01:11:39 +0300

    create a deployment version

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
 
-export const PROXY = "http://localhost:5000";
+export const PROXY = "";
+// export const PROXY = "http://localhost:5000";
 export const PRODUCTS_URL = PROXY + "/api/products";
 export const ORDERS_URL = PROXY + "/api/orders";
